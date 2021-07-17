a62b416559a4ecfb0c35d9896816f6c0d17f1f62
prakashgun <mail.prakash.india@gmail.com>
2021-07-17 08:31:20 +0530

    Move the default category creation to App screen

--- App.tsx
+++ App.tsx
@@ -4,8 +4,10 @@ import 'react-native-gesture-handler'
 import { Connection, getRepository } from 'typeorm/browser'
 import Utils, { DB_CONNECTION_NAME } from './src/common/Utils'
 import AccountContext from './src/context/AccountContext'
+import CategoryContext from './src/context/CategoryContext'
 import DbContext from './src/context/DbContext'
 import { Account } from './src/entities/Account'
+import { Category } from './src/entities/Category'
 import AppNavContainer from './src/navigations/AppNavContainer'
 
 LogBox.ignoreLogs(['Reanimated 2'])
@@ -19,16 +21,24 @@ const App = (props: Props) => {
 
   const [dbConnection, setDbConnection] = useState<Connection | null>(null)
   const [accounts, setAccounts] = useState<Account[]>([])
+  const [categories, setCategories] = useState<Category[]>([])
+
 
   const updateAccounts = async () => {
     const accountRepository = await getRepository(Account, DB_CONNECTION_NAME)
-    setAccounts(await accountRepository.find({ take: 5 }))
+    setAccounts(await accountRepository.find({ take: 10000 }))
   }
+
+  const updateCategories = async () => {
+    const categoryRepository = await getRepository(Category, DB_CONNECTION_NAME)
+    setCategories(await categoryRepository.find({ take: 10000 }))
+}
 
   const setUpConnection = useCallback(async () => {
     setDbConnection(await Utils.createConnection())
     await createDefaultAccounts()
     await updateAccounts()
+    await updateCategories()
   }, [])
 
   const createDefaultAccounts = useCallback(async () => {
@@ -47,19 +57,93 @@ const App = (props: Props) => {
     }
   }, [])
 
+  const createDefaultCategories = useCallback(async () => {
+
+    const categoryRepository = await getRepository(Category, DB_CONNECTION_NAME)
+
+    const categoriesCount = await categoryRepository.count()
+
+    if (categoriesCount === 0) {
+        const defaultCategories = [
+            {
+                name: 'Food',
+                icon_name: 'fastfood',
+                icon_type: 'material-icons'
+            },
+            {
+                name: 'Clothing',
+                icon_name: 'tshirt',
+                icon_type: 'font-awesome-5'
+            },
+            {
+                name: 'Transportation',
+                icon_name: 'car',
+                icon_type: 'font-awesome'
+            },
+            {
+                name: 'Education',
+                icon_name: 'graduation-cap',
+                icon_type: 'font-awesome'
+            },
+            {
+                name: 'Entertainment',
+                icon_name: 'movie',
+                icon_type: 'material-icons'
+            },
+            {
+                name: 'Social',
+                icon_name: 'group',
+                icon_type: 'font-awesome'
+            },
+            {
+                name: 'Investment',
+                icon_name: 'bar-graph',
+                icon_type: 'entypo'
+            },
+            {
+                name: 'Health',
+                icon_name: 'fitness-center',
+                icon_type: 'material-icons'
+            },
+            {
+                name: 'Medical',
+                icon_name: 'medical-services',
+                icon_type: 'material-icons'
+            },
+            {
+                name: 'Others',
+                icon_name: 'miscellaneous-services',
+                icon_type: 'material-icons'
+            }
+        ]
+
+        await categoryRepository
+        .createQueryBuilder()
+        .insert()
+        .into(Category)
+        .values(defaultCategories)
+        .execute()
+
+        console.log('Default categories saved')
+    }
+}, [])
+
   useEffect(() => {
     if (!dbConnection) {
       setUpConnection()
     } else {
       createDefaultAccounts()
       updateAccounts()
+      createDefaultCategories()
     }
   }, [])
 
   return (
     <DbContext.Provider value={{ dbConnection, setUpConnection }}>
       <AccountContext.Provider value={{ accounts, updateAccounts }}>
+      <CategoryContext.Provider value={{ categories, updateCategories }}>
         <AppNavContainer />
+        </CategoryContext.Provider>
       </AccountContext.Provider>
     </DbContext.Provider>
   )
--- src/navigations/CategoryNavigator.tsx
+++ src/navigations/CategoryNavigator.tsx
@@ -1,116 +1,17 @@
 import { createStackNavigator } from '@react-navigation/stack'
-import React, { useContext, useState } from 'react'
-import { useEffect } from 'react'
+import React from 'react'
 import { StyleSheet } from 'react-native'
-import { getRepository } from 'typeorm/browser'
-import { DB_CONNECTION_NAME } from '../common/Utils'
-import DbContext from '../context/DbContext'
-import CategoryContext from '../context/CategoryContext'
-import { Category } from '../entities/Category'
 import AddCategoryScreen from '../screens/AddCategoryScreen'
 import CategoriesScreen from '../screens/CategoriesScreen'
-import { useCallback } from 'react'
 
 export default function CategoryNavigator() {
     const Stack = createStackNavigator()
-    const [categories, setCategories] = useState<Category[]>([])
-    const { dbConnection, setUpConnection } = useContext(DbContext)
-
-    const updateCategories = async () => {
-        const categoryRepository = await getRepository(Category, DB_CONNECTION_NAME)
-        setCategories(await categoryRepository.find({ take: 100 }))
-    }
-
-
-    const createDefaultCategories = useCallback(async () => {
-
-        const categoryRepository = await getRepository(Category, DB_CONNECTION_NAME)
-
-        const categoriesCount = await categoryRepository.count()
-
-        if (categoriesCount === 0) {
-            const defaultCategories = [
-                {
-                    name: 'Food',
-                    icon_name: 'fastfood',
-                    icon_type: 'material-icons'
-                },
-                {
-                    name: 'Clothing',
-                    icon_name: 'tshirt',
-                    icon_type: 'font-awesome-5'
-                },
-                {
-                    name: 'Transportation',
-                    icon_name: 'car',
-                    icon_type: 'font-awesome'
-                },
-                {
-                    name: 'Education',
-                    icon_name: 'graduation-cap',
-                    icon_type: 'font-awesome'
-                },
-                {
-                    name: 'Entertainment',
-                    icon_name: 'movie',
-                    icon_type: 'material-icons'
-                },
-                {
-                    name: 'Social',
-                    icon_name: 'group',
-                    icon_type: 'font-awesome'
-                },
-                {
-                    name: 'Investment',
-                    icon_name: 'bar-graph',
-                    icon_type: 'entypo'
-                },
-                {
-                    name: 'Health',
-                    icon_name: 'fitness-center',
-                    icon_type: 'material-icons'
-                },
-                {
-                    name: 'Medical',
-                    icon_name: 'medical-services',
-                    icon_type: 'material-icons'
-                },
-                {
-                    name: 'Others',
-                    icon_name: 'miscellaneous-services',
-                    icon_type: 'material-icons'
-                }
-            ]
-
-            await categoryRepository
-            .createQueryBuilder()
-            .insert()
-            .into(Category)
-            .values(defaultCategories)
-            .execute()
-
-            console.log('Default categories saved')
-        }
-    }, [])
-
-    useEffect(() => {
-        if (dbConnection) {
-            createDefaultCategories()
-            updateCategories()
-            console.log('categories:')
-            console.log(categories)
-        } else {
-            console.log('No db connection on category navigator')
-        }
-    }, [])
 
     return (
-        <CategoryContext.Provider value={{ categories, updateCategories }}>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Categories" component={CategoriesScreen} />
-                <Stack.Screen name="AddCategory" component={AddCategoryScreen} />
-            </Stack.Navigator>
-        </CategoryContext.Provider>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Categories" component={CategoriesScreen} />
+            <Stack.Screen name="AddCategory" component={AddCategoryScreen} />
+        </Stack.Navigator>
     )
 }
 
